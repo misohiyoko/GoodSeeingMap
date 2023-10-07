f81fb48da9ea1a1f7d72967350e628f3b691162c
misohiyoko <misohiyoko@gmail.com>
2023-10-07 10:19:00 +0900

    update

--- app/src/main/java/net/gdseeing/goodseeingmap/MainActivity.java
+++ app/src/main/java/net/gdseeing/goodseeingmap/MainActivity.java
@@ -75,40 +75,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        PictureData pictData = new PictureData("asdw","bp", 1.2F,1.4F,"kgj","http://ho","arer");
-        try {
-            Request request = apiController.pictUploadRequest(pictData);
-            apiController.doRequest(request, new ResponseCallback() {
-                @Override
-                public void onComplete(Response response) throws IOException {
-                    ResponseCallback.super.onComplete(response);
-                    Log.i("wae",response.body().string());
-                }
-            });
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-        File uploadFile = null;
-        try {
-            uploadFile = File.createTempFile("upload-sample", "txt");
-            try (BufferedWriter bw = new BufferedWriter(new FileWriter(uploadFile))) {
-                bw.write("test");
-                bw.flush();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            s3Controller.upload(getApplicationContext(), pictData, uploadFile, new StringCallback() {
-                @Override
-                public void onComplete(String str) {
-                    StringCallback.super.onComplete(str);
-                    Log.i("wae",str);
-                }
-            });
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+
         if (ContextCompat.checkSelfPermission(
                 getApplicationContext(),
                 Manifest.permission.ACCESS_FINE_LOCATION
